7dafa77059206a62e6de50b6c61fc4ee3ec610a9
ponzS <148766667+ponzS@users.noreply.github.com>
2025-03-15 14:00:37 +0800

    Delete assets directory

--- assets/opensource-BhGtPqfC.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,u as n,A as r,z as e,I as a,F as c}from"./index-jm_w3ouH.js";const i={},p={style:{"font-size":"20px","font-weight":"bold",margin:"30%"}};function u(o,t){return n(),r("div",null,[e("h1",p,[a(c(o.$t("opensource")),1),t[0]||(t[0]=e("button",{herf:"https://github.com/ponzS"},"https://github.com/ponzS",-1))])])}const l=s(i,[["render",u]]);export{l as default};
